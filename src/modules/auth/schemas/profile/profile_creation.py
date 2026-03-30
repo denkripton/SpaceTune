72a3c89573ef7e1af3da017dfcdfdc68702a65ca
Denis <denkripton@gmail.com>
2026-03-30 21:31:29 +0300

    refactor(auth): inherited from ProfileCreationSchema via BaseSchema

--- src/modules/auth/schemas/profile/profile_creation.py
+++ src/modules/auth/schemas/profile/profile_creation.py
@@ -1,11 +1,13 @@
 from datetime import date
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import Field
+
+from src.utils.schemas.base_schema import BaseSchema
 
 
-class ProfileCreationSchema(BaseModel):
+class ProfileCreationSchema(BaseSchema):
     birth_date: date
     bio: Optional[str]
-    country: Optional[str] = Field(max_length=50)
+    country: Optional[str] = Field(max_length=50, examples=["Ukraine"])
     phone_number: Optional[str] = Field(max_length=50, examples=["+380_99_999_9999"])
