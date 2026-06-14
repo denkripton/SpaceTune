c8cb9f1312b7f8464888439ca118beb96b2e1725
denkripton <denkripton@gmail.com>
2026-06-14 22:31:37 +0300

    feat(auth): added PasswordCreateSchema

---
+++ src/modules/auth/schemas/password/__init__.py
@@ -0,0 +1,5 @@
+from src.modules.auth.schemas.password.create import PasswordCreateSchema
+
+__all__ = [
+    "PasswordCreateSchema"
+]
---
+++ src/modules/auth/schemas/password/create.py
@@ -0,0 +1,27 @@
+import re
+
+from pydantic import field_validator, Field, ValidationInfo
+
+from src.utils.schemas.base_schema import BaseSchema
+
+
+class PasswordCreateSchema(BaseSchema):
+    password: str = Field(min_length=8, max_length=64, examples=["som@Th1ng"])
+    confirm_password: str = Field(min_length=8, max_length=64, examples=["som@Th1ng"])
+
+    @field_validator("password")
+    def validate_password(cls, password: str) -> str:
+        if not re.fullmatch(
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$",
+            password,
+        ):
+            raise ValueError(
+                """Password is invalid. It must contain at least: one lowercase letter, one upper case letter, one digit, one special character. Length: 8-64"""
+            )
+        return password
+
+    @field_validator("confirm")
+    def check_passwords(cls, conf_password: str, info: ValidationInfo):
+        if conf_password != info.data["password"]:
+            raise ValueError("Password confirmation is invalid")
+        return conf_password
