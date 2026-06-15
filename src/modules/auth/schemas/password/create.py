3e2b8216252c95eca12c11ef015051bcb0cce6ef
denkripton <denkripton@gmail.com>
2026-06-15 23:57:22 +0300

    refactor(auth): simplifying field_validator in PasswordCreateSchema

--- src/modules/auth/schemas/password/create.py
+++ src/modules/auth/schemas/password/create.py
@@ -3,7 +3,7 @@ import re
 from pydantic import field_validator, Field, ValidationInfo
 
 from src.utils.schemas.base_schema import BaseSchema
-
+from src.modules.auth.utils.password_validation import password_validation
 
 class PasswordCreateSchema(BaseSchema):
     password: str = Field(min_length=8, max_length=64, examples=["som@Th1ng"])
@@ -11,14 +11,7 @@ class PasswordCreateSchema(BaseSchema):
 
     @field_validator("password")
     def validate_password(cls, password: str) -> str:
-        if not re.fullmatch(
-            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$",
-            password,
-        ):
-            raise ValueError(
-                """Password is invalid. It must contain at least: one lowercase letter, one upper case letter, one digit, one special character. Length: 8-64"""
-            )
-        return password
+        return password_validation(password=password)
 
     @field_validator("confirm_password")
     def check_passwords(cls, conf_password: str, info: ValidationInfo):
