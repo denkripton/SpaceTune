48dbada34639cbf7d83b7c4d09e4b25aa284cb3a
denkripton <denkripton@gmail.com>
2026-08-04 23:08:35 +0300

    refactor(src): deleted RedactionStrategy

--- backend/src/utils/interfaces/redaction_strategy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class RedactionStrategy(ABC):
-
-    @abstractmethod
-    def is_sensitive_key(self):
-        raise NotImplementedError("Method must be redefined")
