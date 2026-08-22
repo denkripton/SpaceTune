d1d36bc1887e05def18d3e4ceb11e929bdd5584d
denkripton <denkripton@gmail.com>
2026-08-22 23:42:32 +0300

    feat(auth): added get_current_user_obj()

---
+++ backend/src/modules/auth/dependencies/user_obj.py
@@ -0,0 +1,18 @@
+import uuid
+
+from fastapi import Depends, HTTPException
+
+from src.modules.auth.dependencies.get_current_user import get_current_user
+from src.modules.auth.dependencies.user_repo import user_repository
+from src.modules.auth.models import User
+from src.modules.auth.repository import UserRepository
+
+
+async def get_current_user_obj(
+    user_id: str = Depends(get_current_user),
+    user_repo: UserRepository = Depends(user_repository),
+) -> User:
+    user = await user_repo.get_by_id(id=uuid.UUID(user_id))
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not authorized")
+    return user
