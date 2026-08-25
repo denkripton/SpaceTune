f6d16aff0d360a4c5ddaf08dce2d43831ac2c844
denkripton <denkripton@gmail.com>
2026-08-25 22:23:15 +0300

    refactor(utils): FileSizeLimitExceeded now has a dedicated status code and is inherited from ServiceError

--- backend/src/utils/exceptions/size_limit.py
+++ backend/src/utils/exceptions/size_limit.py
@@ -1,2 +1,6 @@
-class FileSizeLimitExceeded(Exception):
-    pass
+from src.utils.exceptions.service_error import ServiceError
+
+
+class FileSizeLimitExceeded(ServiceError):
+    def __init__(self, msg: str = "Content Too Large", code: int = 413):
+        super().__init__(msg, code)
