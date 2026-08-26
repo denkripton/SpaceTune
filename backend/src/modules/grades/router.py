c762f5e9229c51b24022028b4ad3ae741229b0cc
denkripton <denkripton@gmail.com>
2026-08-26 19:48:34 +0300

    docs(grades): error status codes have been adapted in the grades module

--- backend/src/modules/grades/router.py
+++ backend/src/modules/grades/router.py
@@ -5,10 +5,10 @@ from fastapi import APIRouter, Depends, Form
 from src.modules.auth.dependencies import get_current_user_obj
 from src.modules.auth.models import User
 from src.modules.auth.schemas.exceptions.user_401 import User401
-from src.modules.auth.schemas.exceptions.user_422 import User422
+from src.modules.auth.schemas.exceptions.user_404 import User404
 from src.modules.grades.dependencies import get_grade_service
 from src.modules.grades.service import GradeService
-from src.modules.music.schemas.exceptions.track_422 import Track422
+from src.modules.music.schemas.exceptions.track_404 import Track404
 from src.utils.routing.error_handling import ErrorHandlingRoute
 
 grade_router = APIRouter(prefix="/grades", route_class=ErrorHandlingRoute)
@@ -21,7 +21,7 @@ grade_router = APIRouter(prefix="/grades", route_class=ErrorHandlingRoute)
     tags=["Grades CRUD's"],
     responses={
         401: {"model": User401},
-        422: {"model": Union[Track422, User422]},
+        404: {"model": Union[User404, Track404]},
     },
 )
 async def place_grade(
